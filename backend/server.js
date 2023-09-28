91feb0d98d164c6dbe7913e76931e43562c7ba50
Imesh Pasinda <imeshpasinda@gmail.com>
2023-09-28 15:26:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -37,26 +37,26 @@ app.get("/", (req, res) => {
     ];
 
     const authorList = authors.map(author => `
-        <li>
-            <a href="${author.github}" target="_blank">${author.name}</a>
-        </li>
+        <p class="author">
+            <a href="${author.github}" target="_blank" style="text-decoration: none; color: inherit;">${author.name}</a>
+        </p>
     `).join('');
 
     const mitLicenseText = `
     MIT License
 
     Copyright (c) 2023 SriPass
-
+    
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
     in the Software without restriction, including without limitation the rights
     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
     copies of the Software, and to permit persons to whom the Software is
     furnished to do so, subject to the following conditions:
-
+    
     The above copyright notice and this permission notice shall be included in all
     copies or substantial portions of the Software.
-
+    
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -64,73 +64,84 @@ app.get("/", (req, res) => {
     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.
+    
     `;
 
     const responseMessage = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <style>
-            body {
-                font-family: 'Helvetica', sans-serif;
-                background-color: #f5f5f5;
-                margin: 0;
-                padding: 0;
-            }
-            .container {
-                text-align: center;
-                margin: 50px auto;
-                max-width: 800px;
-                background-color: #fff;
-                padding: 20px;
-                border-radius: 10px;
-                box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-            }
-            .title {
-                font-size: 32px;
-                font-weight: bold;
-                color: #333;
-            }
-            .authors {
-                list-style-type: none;
-                padding: 0;
-            }
-            .authors li {
-                margin: 10px 0;
-                font-size: 18px;
-                color: #555;
-            }
-            .license {
-                margin-top: 20px;
-                white-space: pre-line;
-                font-size: 14px;
-                color: #777;
-            }
-            .active {
-                color: green;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-        <img src="https://github.com/SriPass/SriPass-WEB-APP-BACKEND/blob/main/logo.png?raw=true" alt="Logo" width="150"> <!-- Add this line for the logo -->
-            <div class="title">SriPass Backend Server</div>
-            <h2><span class="active">Active</span></h2>
-            <h2>Code Authors:</h2>
-            <ul class="authors">
-                ${authorList}
-            </ul>
-            <div class="license">
-                ${mitLicenseText}
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <style>
+                body {
+                    font-family: 'Helvetica', sans-serif;
+                    background-color: #f5f5f5;
+                    margin: 0;
+                    padding: 0;
+                }
+                .container {
+                    text-align: center;
+                    margin: 50px auto;
+                    max-width: 800px;
+                    background-color: #fff;
+                    padding: 20px;
+                    border-radius: 10px;
+                    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+                }
+                .title {
+                    font-size: 10px;
+                    font-weight: bold;
+                    color: #333;
+                    margin-bottom: 20px;
+                }
+                .status {
+                    font-size: 16px;
+                    color: green;
+                    margin-bottom: 10px;
+                }
+                .author {
+                    font-size: 14px;
+                    color: #555;
+                    margin: 5px 0;
+                }
+                .license {
+                    margin-top: 20px;
+                    white-space: pre-line;
+                    font-size: 12px;
+                    color: #777;
+                }
+                .logo {
+                    display: block;
+                    margin: 0 auto;
+                    width: 150px;
+                }
+                .repo-link {
+                    font-size: 12px; /* Reduce the font size for the repository link */
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <img class="logo" src="https://github.com/SriPass/SriPass-WEB-APP-BACKEND/blob/main/logo.png?raw=true" alt="Logo">
+                <div class="title">SriPass Backend Server (c) 2023 SriPass</div>
+                <div class="status">Active</div>
+                <h5>Code Authors</h5>
+                <div class="authors">
+                    ${authorList}
+                </div>
+                <div class="license">
+                    ${mitLicenseText}
+                </div>
+                <p class="repo-link">GitHub Repository: <a href="https://github.com/SriPass/SriPass-WEB-APP-BACKEND" target="_blank">https://github.com/SriPass/SriPass-WEB-APP-BACKEND</a></p>
             </div>
-            <p>GitHub Repository: <a href="https://github.com/SriPass/SriPass-WEB-APP-BACKEND" target="_blank">https://github.com/SriPass/SriPass-WEB-APP-BACKEND</a></p> <!-- Add this line for the GitHub repo link -->
-        </div>
-    </body>
-    </html>
+        </body>
+        </html>
     `;
 
     res.send(responseMessage);
 });
+
+
+
 
 
 
